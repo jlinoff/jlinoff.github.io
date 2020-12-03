10bd0ebd5f42715845b06478d84cf9e834e179e6
jlinoff <jlinoff@versive.com>
2020-12-02 21:28:48 -0800

    qspm-0.1.0

---
+++ qspm/js/version.js
@@ -0,0 +1,5 @@
+// Automatically generated from VERSION.
+window.addEventListener("load", function(evt) {
+    document.getElementById("version").innerHTML = "0.1.0";
+    window.qspmVersion = "0.1.0";
+});
