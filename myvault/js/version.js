0a076718cd06de7b17420b83de395a9bfbfe98d9
jlinoff <jlinoff@versive.com>
2021-07-04 15:00:23 -0700

    fix some spelling errors in help

--- myvault/js/version.js
+++ myvault/js/version.js
@@ -3,6 +3,6 @@
  * @module version
  */
 export var VERSION = '0.1.26'
-export var BUILD = '2021-07-04T14:05:15-07:00'
-export var GIT_COMMIT_ID = '778a9c4'
+export var BUILD = '2021-07-04T14:58:48-07:00'
+export var GIT_COMMIT_ID = '162c2b0'
 export var GIT_BRANCH = '20210703-jsdoc'
